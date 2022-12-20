38d197af1dcfca1e5f5155b8b7e5b681986a99bb
haliwt <jonewang@126.com>
2022-12-20 20:26:16 +0800

    Update

--- Core/Inc/cmd_link.h
+++ Core/Inc/cmd_link.h
@@ -3,8 +3,8 @@
 #include "main.h"
 
 
-#define USART1_MAX_RECV_LEN			50					//×î´ó½ÓÊÕ»º´æ×Ö½ÚÊý
-#define USART1_MAX_SEND_LEN			50					//×î´ó·¢ËÍ»º´æ×Ö½ÚÊý
+#define USART1_MAX_RECV_LEN			20					//×î´ó½ÓÊÕ»º´æ×Ö½ÚÊý
+#define USART1_MAX_SEND_LEN			20					//×î´ó·¢ËÍ»º´æ×Ö½ÚÊý
 #define USART11_RX_EN 				1					//0,²»½ÓÊÕ;1,½ÓÊÕ.
 
 extern uint8_t  USART1_RX_BUF[USART1_MAX_RECV_LEN]; 		//½ÓÊÕ»º³å,×î´óUSART3_MAX_RECV_LEN×Ö½Ú
